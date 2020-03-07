9f7104fbe1ab3359cc88e811a18d0e978121cea8
Luis Estraño <luisestrano24@gmail.com>
2020-03-06 22:54:50 -0400

    added firebase config and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { IconsProviderModule } from './icons-provider.module';
+// import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { registerLocaleData } from '@angular/common';
+// import en from '@angular/common/locales/en';
+
+// registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +21,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NzGridModule,
+    NzLayoutModule
   ],
-  providers: [],
+  providers: [
+    // { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
